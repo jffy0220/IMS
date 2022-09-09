b372e15f5111e8dc29ab057508491fea99c278cc
Justin Farley <justin.farley94@gmail.com>
2022-09-09 16:49:05 -0400

    INIT w/ SQL

---
+++ ims/V001_SQL.sql
@@ -0,0 +1,176 @@
+CREATE TABLE `address` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `address_1` varchar(45) DEFAULT NULL,
+  `address_2` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `zip` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `customer` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `phone_1` varchar(45) DEFAULT NULL,
+  `email_1` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `order` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_number` varchar(45) DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `order_type_id` int DEFAULT NULL,
+  `order_status_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `order_line_items` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_id` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `order_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `order_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `date_created` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `order_type_status` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `order_type_id` int DEFAULT NULL,
+  `order_step` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `return` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `employee_id` int DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `item_id` int DEFAULT NULL,
+  `returned_serial_number` varchar(45) DEFAULT NULL,
+  `date_returned` datetime DEFAULT NULL,
+  `return_type_id` int DEFAULT NULL,
+  `process_procedure_id` int DEFAULT NULL,
+  `customer_notes` mediumtext,
+  `employee_notes` mediumtext,
+  `order_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `return_process_procedure` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` mediumtext,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_deleted` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `return_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` mediumtext,
+  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_deleted` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `item` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(45) NOT NULL,
+    `description_1` varchar(255) DEFAULT NULL,
+    `description_2` varchar(255) DEFAULT NULL,
+    `item_value` DECIMAL(11 , 4) DEFAULT NULL,
+    `supplier_1_id` int DEFAULT NULL,
+    `supplier_2_id` int DEFAULT NULL,
+    `item_type_id` int DEFAULT NULL,
+    `model_line_id` int DEFAULT NULL,
+    `version` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `item_details` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `item_id` int NOT NULL,
+    `serial_number` varchar(255) DEFAULT NULL,
+    `supplier_id` int DEFAULT NULL,
+    `po_id` int DEFAULT NULL,
+    `date_received` DATETIME DEFAULT NULL,
+    `order_id` int DEFAULT NULL,
+    `date_shipped` DATETIME DEFAULT NULL,
+    `carrier_id` int DEFAULT NULL,
+    `date_delivered` DATETIME DEFAULT NULL,
+    `expiration` DATETIME DEFAULT NULL,
+    `barcode_1` varchar(255) DEFAULT NULL,
+    `barcode_2` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `item_type` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `description` varchar(255) DEFAULT NULL,
+    `date_created` DATETIME DEFAULT current_timestamp,
+    `date_deleted` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `supplier` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `poc_1_id` int DEFAULT NULL,
+    `poc_2_id` int DEFAULT NULL,
+    `business_address_id` int DEFAULT NULL,
+    `date_created` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `date_deleted` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `purchase_order` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `po_number` varchar(255) NOT NULL,
+    `date_created` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `employee_id` INT NOT NULL,
+    `supplier_id` INT NOT NULL,
+    `shipping_address` varchar(255) NOT NULL,
+    `receiving_address` varchar(255) NOT NULL,
+    `date_expected` DATETIME DEFAULT NULL,
+    `date_delivered` DATETIME DEFAULT NULL,
+    `date_received` DATETIME DEFAULT NULL,
+    `receiver_id` int DEFAULT NULL,
+    `number_items` int DEFAULT NULL,
+    `item_value` DECIMAL(11,4) DEFAULT 0.0,
+    `shipping_cost` DECIMAL(11,4) DEFAULT 0.0,
+    `taxes` DECIMAL(11,4) DEFAULT 0.0,
+    `total_value` DECIMAL(11,4) DEFAULT 0.0,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `receiver` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `employee_id` int NOT NULL,
+    `isActive` TINYINT NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `purchase_order_line_item` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `item_id` int NOT NULL,
+    `serial_number` varchar(255) DEFAULT NULL,
+    `date_shipped` DATETIME DEFAULT NULL,
+    `date_expected` DATETIME DEFAULT NULL,
+    `date_received` DATETIME DEFAULT NULL,
+    `item_condition` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
